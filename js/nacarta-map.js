c436d4dd02d8ee7dcf845208e96a12e78d4d75fd
Andreas Rümpel <ruempel@gmail.com>
2019-07-02 15:38:04 +0200

    Improve processing structure

--- js/nacarta-map.js
+++ js/nacarta-map.js
@@ -10,71 +10,79 @@ export default class NacartaMap {
      * Initializes Google map.
      */
     static init() {
+        // init common data structures
+        NacartaMap.persons = [];
+        NacartaMap.locations = [];
+
+        // init map-specific data structures
         const mapOptions = {
             zoom: 8,
             center: new google.maps.LatLng(51.051, 13.735),
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-
         const canvas = jQuery("<div>").attr({id: "map-canvas"}).appendTo("body");
-        this.map = new google.maps.Map(canvas.get(0), mapOptions);
-        this.bb = new google.maps.LatLngBounds();
-        this.persons = [];
-        this.locations = [];
+        NacartaMap.map = new google.maps.Map(canvas.get(0), mapOptions);
+        NacartaMap.bb = new google.maps.LatLngBounds();
 
-        NacartaMap.loadPersons();
+        NacartaMap.loadPersons(NacartaMap.processMap);
     }
 
     /**
      * Collects locations of persons database.
      */
-    static loadPersons() {
+    static loadPersons(callback) {
         const promises = [];
         for (const file of NacartaMap.config.files)
             promises.push(jQuery.getJSON(NacartaMap.config.basePath + file + ".json",
-                data => this.persons.push(...data)));
+                data => NacartaMap.persons.push(...data)));
 
-        Promise.all(promises).then(() => { // wait for all JSON files processed
-            for (const person of this.persons) {
-                if (person.birth && person.birth.location) this.locations.push(person.birth.location);
-                if (person.death && person.death.location) this.locations.push(person.death.location);
-                // TODO make birth and death locations selectable individually
+        Promise.all(promises).then(callback); // wait for all JSON files processed
+    }
+
+    /**
+     * Renders markers for each event location on a map.
+     */
+    static processMap() {
+        for (const person of NacartaMap.persons) { // collect locations from events
+            if (person.birth && person.birth.location) NacartaMap.locations.push(person.birth.location);
+            if (person.death && person.death.location) NacartaMap.locations.push(person.death.location);
+            // TODO make birth and death locations selectable individually
+            // TODO add events of other types
+        }
+
+        const locationsUnique = Array.from(new Set(NacartaMap.locations));
+        console.info(locationsUnique);
+
+        // try to get geo-coded address from cache
+        const locationsUncached = [];
+        for (const locationRaw of locationsUnique) {
+            const location = locationRaw.trim().replace(/\?/, "");
+            const found = NacartaMap.geocodeCache.find(element => element.name === location);
+            if (found) {
+                const latlng = new google.maps.LatLng(found.lat, found.lng);
+                NacartaMap.handleLocation(latlng, location);
+            } else locationsUncached.push(location);
+        }
+
+        // get remaining locations live and log them to add to cache
+        const interval = window.setInterval(function () {
+            if (locationsUncached.length < 1) {
+                window.clearInterval(interval);
+                return;
             }
 
-            const locationsUnique = Array.from(new Set(this.locations));
-            console.info(locationsUnique);
-
-            // try to get geo-coded address from cache
-            const locationsUncached = [];
-            for (const locationRaw of locationsUnique) {
-                const location = locationRaw.trim().replace(/\?/, "");
-                const found = NacartaMap.geocodeCache.find(element => element.name === location);
-                if (found) {
-                    const latlng = new google.maps.LatLng(found.lat, found.lng);
-                    NacartaMap.handleLocation(latlng, location);
-                } else locationsUncached.push(location);
-            }
-
-            // get remaining locations live and log them to add to cache
-            const interval = window.setInterval(function () {
-                if (locationsUncached.length < 1) {
-                    window.clearInterval(interval);
-                    return;
+            const location = locationsUncached.pop();
+            new google.maps.Geocoder().geocode({"address": location}, (results, status) => {
+                if (status === google.maps.GeocoderStatus.OK) {
+                    const coordinates = results[0].geometry.location;
+                    console.info('ADD THIS ENTRY TO config/geocode-cache.json: "'
+                        + location + '": ' + JSON.stringify(coordinates) + ",");
+                    NacartaMap.handleLocation(coordinates, location);
+                } else {
+                    console.warn("geocoding status = " + status + " for location: " + location);
                 }
-
-                const location = locationsUncached.pop();
-                new google.maps.Geocoder().geocode({"address": location}, (results, status) => {
-                    if (status === google.maps.GeocoderStatus.OK) {
-                        const coordinates = results[0].geometry.location;
-                        console.info('ADD THIS ENTRY TO config/geocode-cache.json: "'
-                            + location + '": ' + JSON.stringify(coordinates) + ",");
-                        handleLocation(coordinates, location);
-                    } else {
-                        console.warn("geocoding status = " + status + " for location: " + location);
-                    }
-                });
-            }, 2000);
-        });
+            });
+        }, 2000);
     }
 
     /**
@@ -93,7 +101,7 @@ export default class NacartaMap {
                 content: name
             }).open(this.map, marker);
         });
-        this.bb.extend(new google.maps.LatLng(latlng.lat(), latlng.lng()), false);
-        this.map.fitBounds(this.bb);
+        NacartaMap.bb.extend(new google.maps.LatLng(latlng.lat(), latlng.lng()), false);
+        NacartaMap.map.fitBounds(this.bb);
     }
 }
